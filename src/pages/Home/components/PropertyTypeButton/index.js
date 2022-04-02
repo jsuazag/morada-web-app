184db6dd4c91df94522e71d65489ec006452ab51
Jerson Suaza <jerson.suaza@gmail.com>
2022-04-02 12:18:11 -0500

    creating home page

---
+++ src/pages/Home/components/PropertyTypeButton/index.js
@@ -0,0 +1,9 @@
+import { PropertyTypeButtonWrapper } from "./styles";
+import { IoAmericanFootballOutline } from "react-icons/io5";
+
+export const PropertyTypeButton = ({ icon, label }) => (
+    <PropertyTypeButtonWrapper>
+        <IoAmericanFootballOutline />
+        <p>{label}</p>
+    </PropertyTypeButtonWrapper>
+)
