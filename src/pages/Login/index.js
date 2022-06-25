0abd2a50f3e114d69e915469dededff6fbe8d39c
Jerson Suaza <jerson.suaza@gmail.com>
2022-06-25 12:27:19 -0500

    login  & alerts added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -11,41 +11,45 @@ import { ButtonIcon } from "../../components/ButtonIcon";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import { requestHttp } from "../../utils/HttpRequest";
 import { useForm } from "react-hook-form";
+import { showAlert, SW_ICON } from "../../utils/SwAlert";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-
   const [visiblePass, setVisiblePass] = useState(false);
-  const { 
+  const navigate = useNavigate();
+  const {
     register,
     handleSubmit,
-    formState: {
-      errors,
-      isValid
-    }
-  } = useForm({ mode: 'onChange' });
+    formState: { errors, isValid },
+  } = useForm({ mode: "onChange" });
 
   const tooglePasswordVisible = () => {
     setVisiblePass(!visiblePass);
-  }
+  };
 
   const onSubmitLogin = (data) => {
-    console.log('data', data);
+    console.log("data", data);
     loginRequest(data);
-  }
+  };
 
   const loginRequest = async (data) => {
     try {
-      const response = await requestHttp(
-          { 
-            endpoint: '/users/login',
-            body: data 
-          }
-      );
+      const response = await requestHttp({
+        endpoint: "/users/login",
+        body: data,
+      });
       console.log(response);
+      showAlert(
+        "Bienvenido",
+        "Validación correcta",
+        SW_ICON.SUCCESS, 
+        () => { navigate('/') }
+      );
     } catch (error) {
-      console.log('error', error);
+      console.log("error", error);
+      showAlert("Error", "Credenciales incorrectas", SW_ICON.ERROR);
     }
-  }
+  };
 
   return (
     <Page hideMenu>
@@ -55,30 +59,47 @@ export const Login = () => {
         <FormControl>
           <FormControlInput>
             <label>Correo electrónico</label>
-            <input 
-              type="email" 
-              {...register("email", {required: true, pattern: /\S+@\S+\.\S+/ })}
+            <input
+              type="email"
+              {...register("email", {
+                required: true,
+                pattern: /\S+@\S+\.\S+/,
+              })}
             />
-            { errors.email?.type === 'required' && <span>El campo correo es requerido</span> }
-            { errors.email?.type === 'pattern' && <span>Ingrese un correo electrónico valido</span> }
+            {errors.email?.type === "required" && (
+              <span>El campo correo es requerido</span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span>Ingrese un correo electrónico valido</span>
+            )}
           </FormControlInput>
         </FormControl>
         <FormControl>
           <FormControlInput>
             <label>Contraseña</label>
-            <input 
-              type={visiblePass ? "text" : "password" } 
+            <input
+              type={visiblePass ? "text" : "password"}
               {...register("password", { required: true })}
             />
-            { errors.password?.type === 'required' && <span>El campo contraseña es requerido</span> }
+            {errors.password?.type === "required" && (
+              <span>El campo contraseña es requerido</span>
+            )}
           </FormControlInput>
           <FormControlAction>
-            <ButtonIcon icon={visiblePass ? IoEyeOff : IoEye} onPress={tooglePasswordVisible} />
+            <ButtonIcon
+              icon={visiblePass ? IoEyeOff : IoEye}
+              onPress={tooglePasswordVisible}
+            />
           </FormControlAction>
         </FormControl>
         <br />
-        <Button disabled={!isValid} type="submit" onPress={() => {}} label="Ingresar"/>
+        <Button
+          disabled={!isValid}
+          type="submit"
+          onPress={() => {}}
+          label="Ingresar"
+        />
       </form>
     </Page>
-  )
+  );
 };
