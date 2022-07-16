0752b371657157b4fd3e352d4f98dfd5ed85dc26
Jerson Suaza <jerson.suaza@gmail.com>
2022-07-16 09:27:54 -0500

    properties filters with request

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,57 +1,83 @@
 import { useState, useEffect, useContext } from "react";
 import { PropertyTypeButton } from "./components/PropertyTypeButton";
 import { PropertyTypesContainer } from "./styles";
-import { IoBusiness, IoHome, IoMap, IoPrism, IoLocation } from 'react-icons/io5' 
+import {
+  IoBusiness,
+  IoHome,
+  IoMap,
+  IoPrism,
+  IoLocation,
+} from "react-icons/io5";
 import { PropertyCard } from "./components/PropertyCard";
 import { Page } from "../../components/Page";
 import { SampleContext } from "../../contexts/SampleContext";
 import { UserContext } from "../../contexts/UserContext";
+import { HTTP_VERBS, requestHttp } from "../../utils/HttpRequest";
 
+const ALL_PROPERTIES_TYPES = 0;
 const PropertiesTypes = [
-    { id: 1, icon: IoBusiness, label: 'Apartamentos' },
-    { id: 2, icon: IoHome, label: 'Casas' },
-    { id: 3, icon: IoMap, label: 'Lotes' },
-    { id: 4, icon: IoPrism, label: 'Fincas' },
-    { id: 5, icon: IoLocation, label: 'Locales' },
+  { id: 1, icon: IoBusiness, label: "Apartamentos" },
+  { id: 2, icon: IoHome, label: "Casas" },
+  { id: 3, icon: IoMap, label: "Lotes" },
+  { id: 4, icon: IoPrism, label: "Fincas" },
+  { id: 5, icon: IoLocation, label: "Locales" },
 ];
 
 export const Home = () => {
+  const [propertyTypeSelected, setPropertyTypeSelected] = useState(ALL_PROPERTIES_TYPES);
+  const { user, setUser } = useContext(UserContext);
+  const [properties, setProperties] = useState([]);
 
-    const [propertyTypeSelected, setPropertyTypeSelected] = useState(1);
-    const {user, setUser} = useContext(UserContext);
+  const propertyTypeHandler = (idNewSelected) => {
+    setPropertyTypeSelected(propertyTypeSelected === idNewSelected 
+                                ? ALL_PROPERTIES_TYPES : idNewSelected);
+  };
 
-    const propertyTypeHandler = (id) => {
-        setPropertyTypeSelected(id);
+  useEffect(() => {
+    requestProperties();
+  }, [propertyTypeSelected]);
+
+  const requestProperties = async () => {
+    try {
+      const response = await requestHttp({
+        method: HTTP_VERBS.GET,
+        endpoint: "/properties",
+        params: makePropertiesFilter()
+      });
+      setProperties(response.data.properties);
+    } catch (error) {
+      // TODO
     }
+  };
 
-    useEffect(() => {
-      // acciones a ejecutar  
-      console.log('propertyTypeSelected ', propertyTypeSelected);
-    }, [propertyTypeSelected]);
+  const makePropertiesFilter = () => {
+    const filters = {};
+    if (propertyTypeSelected !== ALL_PROPERTIES_TYPES) {
+      filters['propertyType'] = propertyTypeSelected;
+    }
+    return filters;
+  }
 
-    return (
-        <Page>
-            <h1>home - morada</h1>
-            <h4>{user.name}</h4>
-            <PropertyTypesContainer>
-            {
-                PropertiesTypes.map(item => 
-                    <PropertyTypeButton 
-                        selected={propertyTypeSelected === item.id} 
-                        icon={item.icon} 
-                        label={item.label}
-                        id={item.id}
-                        onPress={propertyTypeHandler}
-                    /> )
-            }
-            </PropertyTypesContainer>
+  return (
+    <Page>
+      <h1>home - morada</h1>
+      <h4>{user.name}</h4>
+      <PropertyTypesContainer>
+        {PropertiesTypes.map((item, key) => (
+          <PropertyTypeButton
+            selected={propertyTypeSelected === item.id}
+            icon={item.icon}
+            label={item.label}
+            id={item.id}
+            onPress={propertyTypeHandler}
+            key={key}
+          />
+        ))}
+      </PropertyTypesContainer>
 
-            <PropertyCard />
-            <PropertyCard />
-            <PropertyCard />
-            <PropertyCard />
-            <PropertyCard />
-            <PropertyCard />
-        </Page>
-    );
+      {
+        properties.map((item, key) => <PropertyCard key={key} /> )
+      }
+    </Page>
+  );
 };
