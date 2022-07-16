86e71cf39f3ede0e1256e35f1014ae283eae44aa
Jerson Suaza <jerson.suaza@gmail.com>
2022-07-16 12:17:21 -0500

    changes format file

--- src/pages/Property/index.js
+++ src/pages/Property/index.js
@@ -2,13 +2,12 @@ import { useParams } from "react-router-dom";
 import { Page } from "../../components/Page";
 
 export const Property = () => {
+  const { idProperty } = useParams();
 
-    const {idProperty} = useParams()
-
-    return (
-        <Page>
-            <h1>Detalle de Propiedad {idProperty}</h1>
-            <p>contenido....</p>
-        </Page>
-    )
-}
+  return (
+    <Page>
+      <h1>Detalle de Propiedad {idProperty}</h1>
+      <p>contenido....</p>
+    </Page>
+  );
+};
